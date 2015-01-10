26eb4f33d7769b8f426e3932b823c58ad48428b0
Stephen Winje <sjwinje@gmail.com>
2015-01-09 20:29:36 -0600

    QFJ using Labels with wrapped text
    The QFJPage is now using Labels with wrapped text rather than buttons
    (that don’t wrap) so the Headings can be used rather than the Reference.

--- Evangelizer/QFJPage.xaml.cs
+++ Evangelizer/QFJPage.xaml.cs
@@ -8,7 +8,7 @@ namespace Evangelizer
 	{	
 		Dictionary<string, Scripture> dictionary =
 			new Dictionary<string, Scripture>();
-
+/*
 		string [] dKey = new string[] {
 			"Isaiah 43:6–7", 
 			"1 Corinthians 10:31", 
@@ -17,8 +17,18 @@ namespace Evangelizer
 			"1 Timothy 1:15", 
 			"Acts 3:19; 16:31", 
 			"What should you do?"};
+*/
+		string [] dKey = new string[] {
+			"1) God created us for his glory", 
+			"2) Every human should live for God's glory", 
+			"3) All of us have failed to glorify God as we should",
+			"4) All of us are subject to God's just condemnation", 
+			"5) God sent his only son Jesus to provide eternal life and joy", 
+			"6) The benefits purchased by the death of Christ belong to those who repent and trust him", 
+			"What should you do?"};
 
-		StackLayout parent;
+		ScrollView scroll;
+		StackLayout stack;
 
 		public QFJPage ()
 		{
@@ -61,48 +71,43 @@ namespace Evangelizer
 					"What should you do?",
 					"Turn from the deceitful promises of sin. Everyone who calls on the name of the Lord will be saved.\nRomans 10:13 (ESV)\n\nRead the Bible.\n\nPray.\n\nAttend a Bible-believing church."));
 
-			parent = new StackLayout ();
+			stack = new StackLayout ();
+			scroll = new ScrollView ();
+
+			stack.Padding = 20;
+			stack.Spacing = 10;
 
 			// Add heading
 			Label heading = new Label { Text = "Quest for Joy", 
 				HorizontalOptions = LayoutOptions.CenterAndExpand,
 				FontAttributes = FontAttributes.Bold };
-			parent.Children.Add (heading);
+			stack.Children.Add (heading);
 
-			// Add buttons
-			Button add;
-			for (int j=0; j<i-1; j++) {
-				add = new Button { Text = dictionary[dKey[j]].Reference() };
-				add.Clicked += OnButtonClicked;
-				parent.Children.Add (add);
+			// Add labels with tap gesture recognizers
+			var tapGestureRecognizer = new TapGestureRecognizer();
+			tapGestureRecognizer.Tapped +=(s,e)=>OnLabelTapped(s,e);
+
+			Label label;
+			for (int j=0; j<i; j++) {
+				label = new Label { Text = dictionary[dKey[j]].Heading() };
+//				add.Clicked += OnButtonClicked;
+				label.GestureRecognizers.Add(tapGestureRecognizer);
+				stack.Children.Add (label);
 			}
 /*
-			add = new Button { Text = dictionary[dKey[1]].Reference() };
+			Button add = new Button { Text = dictionary["What should you do?"].Heading() };
 			add.Clicked += OnButtonClicked;
-			parent.Children.Add (add);
-
-			add = new Button { Text = dictionary["Romans 3:23"].Reference() };
-			add.Clicked += OnButtonClicked;
-			parent.Children.Add (add);
-
-			add = new Button { Text = dictionary["Romans 6:23"].Reference() };
-			add.Clicked += OnButtonClicked;
-			parent.Children.Add (add);
-
-			add = new Button { Text = dictionary["1 Timothy 1:15"].Reference() };
-			add.Clicked += OnButtonClicked;
-			parent.Children.Add (add);
-
-			add = new Button { Text = dictionary["Acts 3:19; 16:31"].Reference() };
-			add.Clicked += OnButtonClicked;
-			parent.Children.Add (add);
+			stack.Children.Add (add);
 */
-			add = new Button { Text = dictionary["What should you do?"].Heading() };
-			add.Clicked += OnButtonClicked;
-			parent.Children.Add (add);
+			scroll.Content = stack;
+			Content = scroll;
 
-			Content = parent;
+		}
 
+		async void OnLabelTapped(object sender, EventArgs args)
+		{
+			Label l = (Label)sender;
+			await this.Navigation.PushAsync(new TestVersePage(dictionary[l.Text]));
 		}
 
 		async void OnButtonClicked(object sender, EventArgs args)
